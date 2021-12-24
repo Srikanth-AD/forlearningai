bfb0aa6a7788aa64408ddcd20356bbfc17f04d58
Srikanth AD <adsrikanth@gmail.com>
2021-12-24 11:00:00 -0800

    Added GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,32 +12,15 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-plugin-google-gtag`,
+      {
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-F84TPWTRKC", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "for_learning_ai",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
+        id: "GTM-N24CLH4",
+        includeInDevelopment: false,
+        enableWebVitalsTracking: true,        
       },
     },
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
